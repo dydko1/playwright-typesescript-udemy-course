be3758c09e33bc05e016dab00a861761e95e5089
dydko <miroslaw.dyduch@gmail.com>
2025-01-31 14:04:59 +0100

    cucumber lessons

--- tests/01_basic/firstTest.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("My first test", async ({ page }) => {
-  await page.goto("https://www.google.pl");
-  await expect(page).toHaveTitle("Google");
-});
